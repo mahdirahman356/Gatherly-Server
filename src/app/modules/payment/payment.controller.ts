cd07655028b08f6d3b739c4fb464e3e37c70b5af
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-12-13 14:57:48 +0600

    fixing the payment system problem

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -5,25 +5,21 @@ import sendResponse from "../../shared/sendResponse";
 import { stripe } from "../../helper/stripe";
 import config from "../../../config";
 
-const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+const handleStripeWebhookEvent = async (req: Request, res: Response) => {
 
     const sig = req.headers["stripe-signature"] as string;
     let event;
     try {
-        event = stripe.webhooks.constructEvent(req.body, sig,  config.webhook_secret_key as string);
+        event = stripe.webhooks.constructEvent(req.body, sig, config.webhook_secret_key as string);
     } catch (err: any) {
         console.error("⚠️ Webhook signature verification failed:", err.message);
         return res.status(400).send(`Webhook Error: ${err.message}`);
     }
-    const result = await PaymentService.handleStripeWebhookEvent(event);
+    await PaymentService.handleStripeWebhookEvent(event);
 
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: 'Webhook req send successfully',
-        data: result,
-    });
-});
+    res.status(200).json({ received: true });
+
+};
 
 export const PaymentController = {
     handleStripeWebhookEvent
